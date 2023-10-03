ce571858d02cd04a9350ec2df2d6fdaf00145d95
OK-Ro <o.robert1994@hotmail.com>
2023-10-03 17:13:28 +0200

    week3-robert

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import CategoryList from "./Component/CategoryList";
 import ProductList from "./Component/ProductList";
 import ProductDetail from "./Component/ProductDetail";
+import { FavoritesProvider } from "./Favorites";
+import FavoritesPage from "./Component/FavoritesPage";
 import Cart from "./Component/Cart";
 
 import "./Component/CategoryList.css";
+import "./Component/FavoritesPage.css";
 import "./Component/ProductDetail.css";
 import "./Component/ProductList.css";
 import "./Component/Cart.css";
@@ -25,40 +28,46 @@ function App() {
   };
 
   return (
-    <Router>
-      <div className="app">
-        <h1>
-          <Link to="/" className="products-link">
-            Products
-          </Link>
-          <Link to="/cart" className="cart-link">
-            Cart ({cart.length})
-          </Link>
-        </h1>
+    <FavoritesProvider>
+      <Router>
+        <div className="app">
+          <h1>
+            <Link to="/" className="products-link">
+              Products
+            </Link>
+            <Link to="/cart" className="cart-link">
+              Cart ({cart.length})
+            </Link>
+            <Link to="/favorites" className="favorites-link">
+              Favorites
+            </Link>
+          </h1>
 
-        <CategoryList onCategoryClick={handleCategoryClick} />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <ProductList
-                selectedCategory={selectedCategory}
-                cart={cart}
-                setCart={setCart}
-              />
-            }
-          />
-          <Route
-            path="/product/:id"
-            element={<ProductDetail cart={cart} setCart={setCart} />}
-          />
-          <Route
-            path="/cart"
-            element={<Cart cart={cart} removeFromCart={removeFromCart} />}
-          />
-        </Routes>
-      </div>
-    </Router>
+          <CategoryList onCategoryClick={handleCategoryClick} />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <ProductList
+                  selectedCategory={selectedCategory}
+                  cart={cart}
+                  setCart={setCart}
+                />
+              }
+            />
+            <Route
+              path="/product/:id"
+              element={<ProductDetail cart={cart} setCart={setCart} />}
+            />
+            <Route
+              path="/cart"
+              element={<Cart cart={cart} removeFromCart={removeFromCart} />}
+            />
+            <Route path="/favorites" element={<FavoritesPage />} />
+          </Routes>
+        </div>
+      </Router>
+    </FavoritesProvider>
   );
 }
 
